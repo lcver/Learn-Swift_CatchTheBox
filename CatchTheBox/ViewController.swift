dadae40702e312ad5a095566d75a761ce8f9a3a1
lcver <crackover1092@gmail.com>
2021-01-23 00:25:49 +0700

    [add] Create Timer

--- CatchTheBox/ViewController.swift
+++ CatchTheBox/ViewController.swift
@@ -8,12 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var count = 10
+    var tmp = 1
+    var timer = Timer()
+    @IBOutlet weak var timerLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func randNow(_ sender: Any) {
+//        randBoxPosition()
+        count = 10
+        timer = Timer.scheduledTimer(timeInterval:  1, target: self, selector: #selector(timerFunction), userInfo: nil, repeats: true)
+        print(tmp)
+    }
+    
+    @objc func timerFunction() {
+        if count == 0 {
+            timer.invalidate()
+        }
+        timerLabel.text = "\(count)s"
+        
+        count -= 1
+    }
+    
+    func randBoxPosition() {
+        var num = Int.random(in: 1...9)
+        if tmp == num {
+            num = Int.random(in: 1...9)
+        }
+        tmp = num
+    }
 }
 
